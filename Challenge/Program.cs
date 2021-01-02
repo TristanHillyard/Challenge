39a7ba703b97aa1d16f3b4ed2ae5eed9517844a5
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-02 11:23:25 -0700

    Add project files.

---
+++ Challenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Challenge
+{
+    class Program
+    {
+        enum DaysOftheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the current day of the week");
+            string today = Console.ReadLine();
+            DayOfWeek day;
+            try
+            {
+                day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), today);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+        }
+    }
+}
